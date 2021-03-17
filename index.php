f38a67c2d8346314c1f48f9d4e1a43012c771360
runacode <59107019+runacode@users.noreply.github.com>
2021-03-17 12:22:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php $company = "EL CESAR RESTAURANT & BAR";
-$phone = "(978) 794-8169";
-	$address1 = "99 ESSEX ST";
-	$address2 = "LAWRENCE, MA 01840";
+<?php $company = "DANTE'S PEAK HEATING & COOLING";
+$phone = "(773) 425-3912";
+	$address1 = "4350 W FULLERTON AVE";
+	$address2 = "CHICAGO, IL 60639";
 ?>
 <!DOCTYPE html>
 <html style="font-size: 16px;">
@@ -74,7 +74,7 @@ $phone = "(978) 794-8169";
                   </div>
                 </div>
                 <h3 class="u-text u-text-7">Contact us</h3>
-                <h6 class="u-text u-text-8">Tell us about your project!</h6>
+                <h6 class="u-text u-text-8">Don't hesitate to contact us!</h6>
               </div>
             </div>
           </div>
